49f590dd8f9d63729d3d311f6c9f438f7d2a7b88
szankacsanad <cs.szanka@gmail.com>
2025-03-31 19:46:03 +0200

    main

--- frontend/vite-project/src/main.tsx
+++ frontend/vite-project/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Main from './pages/Main.tsx'
+import UjKoncertek from './pages/UjKoncertek.tsx'
+import NavBar from './pages/NavBar.tsx'
+
+const router=createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/ujkoncertek",
+    element: <UjKoncertek />,
+  },
+  {
+    path: "/navbar",
+    element: <NavBar />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <NavBar />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
